8ec1052838afd75b93c107efa8f823f68a6b0a04
Peizhen <peizhen.li1@students.mq.edu.au>
2024-03-13 17:33:46 +1100

    modify content style model

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import tensorflow as tf
 from utilities import *
 from func.train import train_loop
 from loss_fn.loss_funcs import content_only_loss
-from models.content_reconstruction import ContentModel
+from models.feature_extraction import FeatureExtractionModel
 import tensorflow_hub as hub
 
-content_image = load_img('assets/MQ_fountain.jpg')
+content_image = load_img('assets/content_image.jpeg') # load_img('assets/MQ_fountain.jpg')
 style_image = load_img('assets/starry_night.jpg')
 
 
@@ -17,11 +17,12 @@ style_image = load_img('assets/starry_night.jpg')
 style_weight = 1e-2
 content_weight = 1e4
 
+
 def run_fast_style_transfer():
 	# fast style transfer using TF-Hub
 	hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
 	stylized_image = hub_model(tf.constant(content_image), tf.constant(style_image))[0]
-	plot_tensor_as_image(stylized_image, save_as='stylized.png')
+	plot_tensor_as_image(stylized_image, save_as='outputs/stylized.png')
 	# tensor_to_image(stylized_image)
 	# img_show = stylized_image * 255
 	# tensor = np.array(tensor, dtype=np.uint8)
@@ -30,18 +31,16 @@ def run_fast_style_transfer():
 def run_content_reconstruction(content_layers):
 	opt = tf.keras.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
 	image = tf.Variable(tf.random.uniform(content_image.shape, dtype=tf.dtypes.float32))
-	model = ContentModel(content_layers)
+	model = FeatureExtractionModel(content_layers)
 	target_features = model(content_image)
 	train_loop(model, image, target_features, content_only_loss, opt, epochs=10, steps_per_epoch=100)
 	# show and save image
-	image_show = image * 255
-	image_show = np.array(image_show, dtype=np.uint8)
-	plt.show(image_show)
-	plt.savefig('content_recon.png')
+	plot_tensor_as_image(image, save_as=f'outputs/content_reconstruction/{content_layers[0]}.png')
 
 
 
 if __name__ == "__main__":
 	print(tf.__version__)
+	# run_fast_style_transfer()
 	content_layers = ['block5_conv2']
 	run_content_reconstruction(content_layers)
